5426e42d5e939b5850aeb3874dabebfe0e41da10
Otávio Paganotti <otaviopaganotti@outlook.com>
2022-07-07 14:56:33 -0400

    remove trecho inutilizado

--- src/apps/dashboard/src/main.ts
+++ src/apps/dashboard/src/main.ts
@@ -1,10 +1,8 @@
 import { createApp } from 'vue';
 import { RouterView } from 'vue-router';
 import router from './router';
-// import App from './layouts/Default.vue';
 
 const app = createApp(RouterView);
-// const app = createApp(App);
 
 app.use(router);
 
